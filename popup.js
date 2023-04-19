4e2877ca5cc10e6f1fdd6fe4e5f24589fb476115
crlsh <crlshgimenez@gmail.com>
2023-04-19 12:19:44 -0300

    its alive!

--- popup.js
+++ popup.js
@@ -1,25 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('mostrar-texto').addEventListener('click', function() {
-      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.scripting.executeScript({
-          target: {tabId: tabs[0].id},
-          func: function() {
-            var paragraphs = document.getElementsByTagName('p');
-            if (paragraphs.length > 0) {
-              for (var i = 0; i < paragraphs.length; i++) {
-                var text = document.createTextNode(paragraphs[i].innerText);
-                paragraphs[i].parentNode.replaceChild(text, paragraphs[i]);
-              }
-            } else {
-              alert('No paragraphs found on this page.');
+  document.getElementById('mostrar-texto').addEventListener('click', function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.scripting.executeScript({
+        target: {tabId: tabs[0].id},
+        func: function() {
+          var paragraphs = document.getElementsByTagName('p');
+          if (paragraphs.length > 0) {
+            var text = '';
+            for (var i = 0; i < paragraphs.length; i++) {
+              text += paragraphs[i].innerText + '<br>';
             }
+            var newBody = document.createElement('div');
+            newBody.innerHTML = text;
+            document.body.parentNode.replaceChild(newBody, document.body);
+          } else {
+            alert('No paragraphs found on this page.');
           }
-        }, function(result) {
-          if (chrome.runtime.lastError) {
-            console.error(chrome.runtime.lastError.message);
-          }
-        });
+        }
+      }, function(result) {
+        if (chrome.runtime.lastError) {
+          console.error(chrome.runtime.lastError.message);
+        }
       });
     });
   });
-  
+});
