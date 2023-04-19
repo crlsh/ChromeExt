e33f80722d948672a6e239a445881cf7735668f2
crlsh <crlshgimenez@gmail.com>
2023-04-19 11:58:33 -0300

    some mods

--- popup.js
+++ popup.js
@@ -1,11 +1,25 @@
-document.getElementById('mostrar-texto').addEventListener('click', function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.scripting.executeScript({
-        target: {tabId: tabs[0].id},
-        func: function() {
-          var texto = document.body.innerText;
-          document.body.innerHTML = '<div>' + texto + '</div>';
-        }
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('mostrar-texto').addEventListener('click', function() {
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.scripting.executeScript({
+          target: {tabId: tabs[0].id},
+          func: function() {
+            var paragraphs = document.getElementsByTagName('p');
+            if (paragraphs.length > 0) {
+              for (var i = 0; i < paragraphs.length; i++) {
+                var text = document.createTextNode(paragraphs[i].innerText);
+                paragraphs[i].parentNode.replaceChild(text, paragraphs[i]);
+              }
+            } else {
+              alert('No paragraphs found on this page.');
+            }
+          }
+        }, function(result) {
+          if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError.message);
+          }
+        });
       });
     });
   });
+  
